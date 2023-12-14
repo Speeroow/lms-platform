9c73eb470247b8a1d7e846012d64793f499354ec
Speeroow <abhishek.rajawat1909@gmail.com>
2023-12-14 15:25:03 +0530

    course creation ui

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image height={40} width={40} alt="logo" src="/logo.svg" />;
+  return <Image height={136} width={136} alt="logo" src="/logo.svg" />;
 };
